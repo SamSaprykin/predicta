0e302923f2af742df13485249678006a1bebb47c
emphil <80403717+emphil@users.noreply.github.com>
2021-04-02 10:51:46 +0300

    Update risk-analysis-e.js

--- src/components/advanced-statistics/risk-analysis-e.js
+++ src/components/advanced-statistics/risk-analysis-e.js
@@ -98,7 +98,7 @@ const TextBox = styled.div`
   }
 `
 const TitleSection = styled.h2`
-  text-transform: capitalize;
+  text-transform: none;
   color: #222B3E;
   letter-spacing: 0px;
   font-size:26px;
